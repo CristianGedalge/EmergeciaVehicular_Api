174175b51357e3711311e8b1e2fff7a9caccc11e
Cristian_Gedalge <gedalge.cristian22@gmail.com>
2026-04-26 00:40:19 -0400

    feat:creacion del modelo solictud y migracoion

--- app/models/solicitud.py
+++ app/models/solicitud.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Text, Numeric, DateTime, ForeignKey, Enum as SQLEnum
+from sqlalchemy import Column, Integer, String, Text, Numeric, DateTime, ForeignKey, Enum as SQLEnum, Float
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.sql import func
 from app.models.base import Base
 import enum
@@ -8,6 +9,7 @@ class EstadoSolicitudEnum(str, enum.Enum):
     PENDIENTE = "PENDIENTE"
     CLASIFICADO = "CLASIFICADO"
     PUBLICADO = "PUBLICADO"
+    ACEPTADO = "ACEPTADO"
     ASIGNADO = "ASIGNADO"
     EN_CAMINO = "EN_CAMINO"
     EN_SITIO = "EN_SITIO"
@@ -15,17 +17,26 @@ class EstadoSolicitudEnum(str, enum.Enum):
     CANCELADO = "CANCELADO"
 
 
-class SolicitudAuxilio(Base):
-    __tablename__ = "solicitud_auxilio"
+class Solicitud(Base):
+    __tablename__ = "solicitud"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     cliente_id = Column(Integer, ForeignKey("usuario.id"), nullable=False)
     vehiculo_id = Column(Integer, ForeignKey("vehiculo.id"), nullable=False)
-    tipo_servicio_id = Column(Integer, ForeignKey("tipo_servicio.id"), nullable=True) # Mapped later by IA
+    taller_id = Column(Integer, ForeignKey("taller.id"), nullable=True)
+    mecanico_id = Column(Integer, ForeignKey("mecanico.id"), nullable=True)
+    tipo_servicio_id = Column(Integer, ForeignKey("tipo_servicio.id"), nullable=True)
+
     descripcion = Column(Text, nullable=True)
-    url_img = Column(Text, nullable=True)
-    url_audio = Column(Text, nullable=True)
+    # Campo tipo Arreglo para múltiples fotos en una sola fila
+    urls_fotos = Column(ARRAY(Text), nullable=True)
+    
     latitud = Column(Numeric(10, 7), nullable=False)
     longitud = Column(Numeric(10, 7), nullable=False)
+    
+    precio_estimado = Column(Float, nullable=True)
+    precio_final = Column(Float, nullable=True)
+
     estado = Column(SQLEnum(EstadoSolicitudEnum), default=EstadoSolicitudEnum.PENDIENTE, nullable=False)
     fecha_creacion = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    fecha_actualizacion = Column(DateTime(timezone=True), onupdate=func.now())
